5759448237dad09f98eb4d83530c5fec3088727e
emanuel5867 <emisor.2009@gmail.com>
2023-07-26 14:30:39 +0300

    nu stiu ce am facut, dar merge bine

--- EAR-3 MyFirstGameJam/Assets/Scripts/CursorManager.cs
+++ EAR-3 MyFirstGameJam/Assets/Scripts/CursorManager.cs
@@ -7,12 +7,22 @@ public class CursorManager : MonoBehaviour
 
     public Texture2D cursorTexture;
     private Vector2 cursorHotspot;
+    RoomFirstDungeonGenerator generator;
+    GameObject scriptGenerator;
 
 
     void Start()
     {
+
         cursorHotspot = new Vector2(cursorTexture.width / 2, cursorTexture.height / 2);
         Cursor.SetCursor(cursorTexture, cursorHotspot, CursorMode.Auto);
+        
     }
-    
+
+    void Awake()
+    {
+        scriptGenerator=GameObject.Find("/GENERARE/RoomFirstMapGenerator");
+        generator = scriptGenerator.GetComponent<RoomFirstDungeonGenerator>();
+        generator.GenerateDungeon();
+    }
 }

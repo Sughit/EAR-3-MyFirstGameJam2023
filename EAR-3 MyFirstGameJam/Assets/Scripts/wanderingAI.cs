e0d69e135a3dee896ff3a24e8bf5aff1dee5383a
emanuel5867 <emisor.2009@gmail.com>
2023-07-29 03:19:59 +0300

    merge!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- EAR-3 MyFirstGameJam/Assets/Scripts/wanderingAI.cs
+++ EAR-3 MyFirstGameJam/Assets/Scripts/wanderingAI.cs
@@ -1,25 +1,145 @@
 using System.Collections;
+
 using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class wanderingAI : MonoBehaviour
+
 {
-    public Transform inamic;
-    public float speed=0.02f;
-    private List<Vector3> Directii = new List<Vector3>
-    {
-        new Vector3(0,1,0),
-        new Vector3(1,0,0),
-        new Vector3(0,-1,0),
-        new Vector3(-1,0,0)
-    };
+
+    public float moveSpeed = 3f;
+
+    public float rotSpeed = 100f;
+
+
+
+    private bool isWandering = false;
+
+    private bool isRotatingLeft = false;
+
+    private bool isRotatingRight = false;
+
+    private bool isWalking = false;
+
+    EnemyScript enemyScript;
+
+    Rigidbody2D rb;
+
+
+
+    // Update is called once per frame
+
     void Start()
     {
-        Debug.Log(Directii[Random.Range(1,Directii.Count)]);
-        inamic = GetComponent<Transform>();
+        enemyScript = this.GetComponent<EnemyScript>();
+         rb = this.GetComponent<Rigidbody2D>();
     }
+
     void Update()
+
     {
-        inamic.Translate(Directii[Random.Range(1,Directii.Count)]*Time.deltaTime* speed);
+        if(enemyScript.inRange==false)
+ {
+        if (isWandering == false)
+
+        {
+
+            StartCoroutine(Wander());
+
+        }
+
+        if (isRotatingRight == true)
+
+        {
+            transform.Rotate(transform.forward * Time.deltaTime * rotSpeed, Space.World);
+
+        }
+
+        if (isRotatingLeft == true)
+
+        {
+
+            transform.Rotate(transform.forward * Time.deltaTime * -rotSpeed, Space.World);
+
+        }
+
+        if (isWalking == true)
+
+        {
+
+            transform.position += transform.right * moveSpeed * Time.deltaTime;
+
+        }
+ }
     }
+
+
+
+    IEnumerator Wander()
+
+    {
+
+        int rotTime = Random.Range(1, 3);
+
+        int rotateWait = Random.Range(1, 3);
+
+        int rotateLorR = Random.Range(1, 2);
+
+        int walkWait = Random.Range(1, 5);
+
+        int walkTime = Random.Range(1, 5);
+
+
+
+        isWandering = true;
+
+
+
+        yield return new WaitForSeconds(walkWait);
+
+        isWalking = true;
+
+        yield return new WaitForSeconds(walkTime);
+
+        isWalking = false;
+
+        yield return new WaitForSeconds(rotateWait);
+
+        if (rotateLorR == 1)
+
+        {
+
+            isRotatingRight = true;
+
+            yield return new WaitForSeconds(rotTime);
+
+            isRotatingRight = false;
+
+        }
+
+        if (rotateLorR == 2)
+
+        {
+
+            isRotatingLeft = true;
+
+            yield return new WaitForSeconds(rotTime);
+
+            isRotatingLeft = false;
+
+        }
+
+        isWandering = false;
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        isWandering = false;
+        rb.velocity = Vector3.zero;
+    }
+
 }

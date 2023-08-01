6435b0215e54ca2bc0eb2bd42ceb1cfadbf3603f
Sughit <andreisorici2007@gmail.com>
2023-08-01 21:40:49 +0300

    schimbat statistici

--- EAR-3 MyFirstGameJam/Assets/Scripts/cartiScripts/doubleStatsCard.cs
+++ EAR-3 MyFirstGameJam/Assets/Scripts/cartiScripts/doubleStatsCard.cs
@@ -20,12 +20,12 @@ public class doubleStatsCard : MonoBehaviour
 
     IEnumerator DoubleStats()
     {
-        speed.boostDuration = 20f;
+        speed.boostDuration = 14f;
         health.healAmount = 40f;
         bulletScript.damageBoost = 80;
         glontInamic.resistenceDamage = 3f;
         yield return new WaitForSeconds(boostDuration);
-        speed.boostDuration = 10f;
+        speed.boostDuration = 7f;
         health.healAmount = 20f;
         bulletScript.damageBoost = 40;
         glontInamic.resistenceDamage = 7f;
